11f2b06ce30b1005fec5d46680c33927582e9020
ashokmachineni <ashok5679m@gmail.com>
2020-05-06 17:24:46 +0100

    displaying detched data in browser withbootstrap

--- index.js
+++ index.js
@@ -7,25 +7,30 @@ const app = express();
 
 //api key pk_e7fd75caea4b405cb200bed5d06ece3c
 //https://cloud.iexapis.com/stable/stock/aapl/quote?token=YOUR_TOKEN_HERE
-request(
-    "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_e7fd75caea4b405cb200bed5d06ece3c", { json: true },
-    (err, res, body) => {
-        if (err) {
-            return console.log(err);
-        }
+function call_api(finishedAPI) {
+    request(
+        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_e7fd75caea4b405cb200bed5d06ece3c", { json: true },
+        (err, res, body) => {
+            if (err) {
+                return console.log(err);
+            }
 
-        if (res.statusCode === 200) {
-            console.log(body);
+            if (res.statusCode === 200) {
+                //console.log(body);
+                finishedAPI(body);
+            }
         }
-    }
-);
+    );
+}
 
 //set handlebars middleware
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
 app.get("/", function(req, res) {
-    res.render("home");
+    call_api(function(doneAPI) {
+        res.render("home", { stock: doneAPI });
+    });
 });
 //about page route
 app.get("/about.html", function(req, res) {
